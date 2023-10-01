1c1fa9980bb339ce5d8ead3c5e1bebed38c466eb
Patiencemugambi <patiencemugambi@gmail.com>
2023-10-01 08:15:21 +0300

    Add user profile route
    - Added a new route for user profiles
    
    This commit adds a new route to display the user profiles

--- server/app.py
+++ server/app.py
@@ -2,4 +2,26 @@
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)   #
+
+
+# Decorators are functions that take other functions as arguments, 
+# extend their behavior in some way, and then return them or another function. 
+# eg @app.route()
+
+# define routes 
+
+@app.route('/')   #(/) base URL 
+def index():      # view - functions that map to the url. they return the rsponse that the client delivers to the user. 
+                  #  eg view index() returns the HTML code 
+    return '<h1>Welcome to my page!</h1>'
+
+# append to server/app.py
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+
+if __name__ == '__main__':
+    app.run(port=5556, debug= True)    
